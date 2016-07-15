3043bcc64701db88a3930a9b0c592c12d2e6b552
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-07-15 05:57:19 -0600

    Improvements

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sleep 5 # Wait for X to start
-if ps aux|grep "ruby $HOME/projects/deskspy/loop.rb"|egrep -v "grep" > /dev/null
+if ps aux|grep "ruby loop.rb"|egrep -v "grep" > /dev/null
 then
   echo "Running"
 else
-  ruby $HOME/projects/deskspy/loop.rb &
+  ruby loop.rb &
 fi
